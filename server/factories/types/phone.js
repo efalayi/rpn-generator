a57d6f87c7fd30037ef0dc3e45890e68d729f39b
Esther Falayi <esther.falayi@andela.com>
2019-01-03 15:22:24 +0100

    fix(new-numbers): generate new numbers
    - fix response status error in contacts.js
    - update phone type function

--- server/factories/types/phone.js
+++ server/factories/types/phone.js
@@ -1,7 +1,15 @@
+const getNonZeroSecondDigitIndex = (nonZeroString) => {
+  const stringArray = nonZeroString.split('')
+  const secondDigitIndex = stringArray.findIndex(digit => digit !== '0')
+  return secondDigitIndex
+}
+
 const phone = (digits) => {
   const randomNumber = Math.random()
   const [zero, nonZero] = randomNumber.toString().split('.')
-  const truncatedNonZero = nonZero.slice(0, digits - 1)
+  const secondDigitIndex = getNonZeroSecondDigitIndex(nonZero)
+  const endIndex = secondDigitIndex + (digits - 1)
+  const truncatedNonZero = nonZero.substring(secondDigitIndex, endIndex)
   const phoneNumber = [zero, ...truncatedNonZero]
   return phoneNumber.join('')
 }
